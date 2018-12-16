9b17ea1f5511303ef1f16fce061f20ba85e636a0
bclzliuwei <bclzliuwei@gmail.com>
2018-12-16 21:34:48 +0000

    for testing

--- Win32ProjMenuTest/Win32ProjMenuTest.cpp
+++ Win32ProjMenuTest/Win32ProjMenuTest.cpp
@@ -10,6 +10,7 @@
 HINSTANCE hInst;								// current instance
 TCHAR szTitle[MAX_LOADSTRING];					// The title bar text
 TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
+HMENU hMenu, hPopupMenu;
 
 // Forward declarations of functions included in this code module:
 ATOM				MyRegisterClass(HINSTANCE hInstance);
